c5d43056826e057454c7b38809aad253e9961ee0
nashdev255 <yutori1107@gmail.com>
2023-09-12 22:11:22 +0900

    profile introduce button

--- src/app/components/Profile.tsx
+++ src/app/components/Profile.tsx
@@ -105,6 +105,27 @@ const Profile = () => {
 
         <div className='mb-5'>
           <div className='text-sm mb-1 font-bold'>自己紹介</div>
+          <textarea
+            className='border rounded-md w-full py-2 px-3 focus:outline-none focus:border-sky-500'
+            placeholder='自己紹介'
+            id='introduce'
+            {...register('introduce')}
+            rows={5}
+          />
+        </div>
+
+        <div className='mb-5'>
+          {loading ? (
+            <Loading />
+          ) : (
+            <button
+              type='submit'
+              className='font-bold bg-sky-500 hover:brightness-95 w-full rounded-full p-2 text-white text-sm'
+            >
+              変更
+            </button>
+          )}
+          {message && <div className='my-5 text-center text-red-500'>{message}</div>}
         </div>
       </form>
     </div>
