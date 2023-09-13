b8d42ff0cb75637a014d8265b6ae248c3abc3add
nashdev255 <yutori1107@gmail.com>
2023-09-13 10:37:41 +0900

    add email change form

---
+++ src/app/components/Email.tsx
@@ -0,0 +1,76 @@
+'use client'
+
+import { useState } from 'react';
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
+import { useRouter } from 'next/navigation';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import Loading from '@/app/Loading';
+import * as z from 'zod';
+import type { Database } from '@/lib/database.types';
+type Schema = z.infer<typeof schema>;
+
+const schema = z.object({
+  email: z.string().email({ message: 'メールアドレスが正しくありません。' }),
+})
+
+const Email = ({ email }: { email: string }) => {
+  const router = useRouter();
+  const supabase = createClientComponentClient<Database>();
+  const [loading, setLoading] = useState(false);
+  const [message, setMessage] = useState('');
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: { email: '' },
+    resolver: zodResolver(schema),
+  })
+
+  const onSubmit: SubmitHandler<Schema> = async () => {
+
+  }
+
+  return (
+    <div>
+      <div className='text-center font-bold text-xl mb-10'>メールアドレス変更</div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className='mb-5'>
+          <div className='text-sm mb-1 font-bold'>現在のメールアドレス</div>
+          <div>{email}</div>
+        </div>
+
+        <div className='mb-5'>
+          <div className='text-sm mb-1 font-bold'>新しいメールアドレス</div>
+          <input
+            type='email'
+            className='border rounded-md w-full py-2 px-3 focus:outline-none focus:border-sky-500'
+            placeholder='新しいメールアドレス'
+            id='email'
+            {...register('email', { required: true })}
+          />
+          <div className='my-3 text-center text-sm text-red-500'>{errors.email?.message}</div>
+        </div>
+
+        <div className='mb-5'>
+          {loading ? (
+            <Loading />
+          ) : (
+            <button
+              type='submit'
+              className='font-bold bg-sky-500 hover:brightness-95 w-full rounded-full p-2 text-white text-sm'
+            >
+              変更
+            </button>
+          )}
+
+          {message && <div className='my-5 texk-center text-sm text-red-500'>{message}</div>}
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default Email;
