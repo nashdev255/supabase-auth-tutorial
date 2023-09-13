15170f3b1cced4fa5d0b910b93b6caa606ada917
nashdev255 <yutori1107@gmail.com>
2023-09-13 10:08:58 +0900

    add email-change

--- src/app/components/Navigation.tsx
+++ src/app/components/Navigation.tsx
@@ -29,12 +29,19 @@ const Navigation = ({ session, profile }: { session: Session | null, profile: Pr
         </Link>
         <div className='text-sm font-bold'>
           {session ? (
-            <div className='flex items-center space-x-5'>
+            <div className='flex itmes-center space-x-5'>
               <Link href={'/settings/profile'}>
-                <div>プロフィール</div>
+                <div className='relative w-10 h-10'>
+                  <Image
+                    src={profile && profile.avatar_url ? profile.avatar_url : '/default.png'}
+                    className='rounded-full object-cover'
+                    alt='avatar'
+                    fill
+                  />
+                </div>
               </Link>
             </div>
-          ): (
+          ) : (
             <div className='flex items-center space-x-5'>
               <Link href={'/auth/login'}>ログイン</Link>
               <Link href={'/auth/signup'}>サインアップ</Link>
